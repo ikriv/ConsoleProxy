526855703bd511a4cd50ac65b6028b155d9f1b38
Ivan Krivyakov <ivan@ikriv.com>
2019-03-03 16:51:40 -0500

    Exec: added options for process creation flags

--- Exec/Exec.cpp
+++ Exec/Exec.cpp
@@ -9,7 +9,15 @@ static HANDLE hProcess = GetCurrentProcess();
 
 void usage()
 {
-	puts("Usage: exec command OR exec \"command arg1 arg2 ...\"");
+	puts("Usage: exec [options] command");
+	puts("If command contain spaces, enclose it in quotes");
+	puts("Options:");
+	puts("\t-c CREATE_NEW_CONSOLE");
+	puts("\t-d DETACHED_PROCESS");
+	puts("\t-w CREATE_NO_WINDOW");
+	puts("\t-0 no flags");
+	puts("");
+	puts("Without any options an intelligent default is used to ensure proper output redirection");
 }
 
 HANDLE duplicate(HANDLE h)
@@ -22,27 +30,46 @@ HANDLE duplicate(HANDLE h)
 int wmain(int argc, wchar_t** argv)
 {
 	// argv[0] is the process name
-	if (argc != 2)
+	if (argc != 2 && argc != 3)
 	{
 		usage();
 		return ERROR_INVALID_DATA;
 	}
 
-	LPWSTR command = argv[1];
-	DWORD processFlags = 0;
+	bool hasOption = false;
+	DWORD flagsFromOption = 0;
+
+	if (argc == 3)
+	{
+		hasOption = true;
+		wchar_t* option = argv[1];
+		if (wcscmp(L"-c", option) == 0) flagsFromOption = CREATE_NEW_CONSOLE;
+		else if (wcscmp(L"-d", option) == 0) flagsFromOption = DETACHED_PROCESS;
+		else if (wcscmp(L"-w", option) == 0) flagsFromOption = CREATE_NO_WINDOW;
+		else if (wcscmp(L"-0", option) == 0) flagsFromOption = 0;
+		else
+		{
+			usage();
+			return ERROR_INVALID_DATA;
+		}
+	}
+
+	LPWSTR command = argv[hasOption ? 2 : 1];
+	DWORD processFlags = flagsFromOption;
 
 	STARTUPINFO si;
 	ZeroMemory(&si, sizeof(si));
 	si.cb = sizeof(si);
 	
-	if (!GetConsoleWindow())
+	if (!hasOption && !GetConsoleWindow())
 	{
-		// There is no console, so we were started as either DETACHED_PROCESS or CREATE_NO_WINDOW.
-		// This is important for handling non-ASCII output
+		// Process flags option not specified and there is no console
+		// Current process is started with either DETACHED_PROCESS or CREATE_NO_WINDOW.
+		// The distinction is important for handling non-ASCII output
 		// If we are DETACHED_PROCESS, we will have system code page CP_ACP, CP 1251 for Cyrillic
 		// If we are CREATE_NO_WINDOW, we will have OEM code page CP_OEMCP, CP 866 for Cyrillic
 		// 
-		// We want the child process to have the same code page as us, or the output will be garbled.
+		// We want the child process to have the same I/O handles and the same code page as us, or the output will be garbled.
 		// Thus, we run it detached if we are detached, and "no window" otherwise
 		bool weAreDetached = GetConsoleOutputCP() == CP_ACP; 
 		processFlags = weAreDetached ? DETACHED_PROCESS : CREATE_NO_WINDOW;
