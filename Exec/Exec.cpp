ef165126511e0726249e8e7e36218a449a8f5bff
Ivan Krivyakov <ivan@ikriv.com>
2019-03-07 22:43:28 -0500

    Exec: added more options, change default handling

--- Exec/Exec.cpp
+++ Exec/Exec.cpp
@@ -10,15 +10,18 @@ static HANDLE hProcess = GetCurrentProcess();
 
 void usage()
 {
-	puts("Usage: exec [options] command");
-	puts("If command contain spaces, enclose it in quotes");
+	puts("Usage: exec [-options] command");
+	puts("Example: exec -arw cmd /c echo foo");
 	puts("Options:");
-	puts("\t-c CREATE_NEW_CONSOLE");
-	puts("\t-d DETACHED_PROCESS");
-	puts("\t-w CREATE_NO_WINDOW");
-	puts("\t-0 no flags");
+	puts("\ta Set ANSI code page (CP_ACP)");
+	puts("\tc Use CREATE_NEW_CONSOLE");
+	puts("\td Use DETACHED_PROCESS");
+	puts("\tr Explicitly redirect child's handles");
+	puts("\tw Use CREATE_NO_WINDOW");
+	puts("\t0 Use no special flags");
 	puts("");
-	puts("Without any options an intelligent default is used to ensure proper output redirection");
+	puts("c,d,w,0 are mutually exclusive");
+	puts("Default options are -rw when Exec process itself is detached, and -0 in all other cases");
 }
 
 HANDLE duplicate(HANDLE h)
@@ -28,60 +31,102 @@ HANDLE duplicate(HANDLE h)
 	return result;
 }
 
+struct Options
+{
+	bool HasOptions;
+	bool ForceAnsiCodepage;
+	DWORD Flags;
+	bool ForceRedirect;
+};
+
+bool getOptions(wchar_t* arg, Options* pOptions)
+{
+	if (*arg != '-') return true;
+	pOptions->HasOptions = true;
+
+	while (*++arg)
+	{
+		switch (*arg)
+		{
+		case 'a': pOptions->ForceAnsiCodepage = true; break;
+		case 'c': pOptions->Flags = CREATE_NEW_CONSOLE; break;
+		case 'd': pOptions->Flags = DETACHED_PROCESS; break;
+		case 'r': pOptions->ForceRedirect = true; break;
+		case 'w': pOptions->Flags = CREATE_NO_WINDOW; break;
+		case '0': pOptions->Flags = 0; break;
+		default: return false; // unknown option
+		}
+	}
+
+	return true;
+}
+
 int wmain(int argc, wchar_t** argv)
 {
 	// argv[0] is the process name
-	bool hasOption = false;
 	DWORD flagsFromOption = 0;
+
+	Options options = {};
 
 	// check if argv[1] is an option
 	if (argc > 2)
 	{
-		hasOption = true;
-		wchar_t* option = argv[1];
-		if (wcscmp(L"-c", option) == 0) flagsFromOption = CREATE_NEW_CONSOLE;
-		else if (wcscmp(L"-d", option) == 0) flagsFromOption = DETACHED_PROCESS;
-		else if (wcscmp(L"-w", option) == 0) flagsFromOption = CREATE_NO_WINDOW;
-		else if (wcscmp(L"-0", option) == 0) flagsFromOption = 0;
+		if (!getOptions(argv[1], &options)) return 1;
+	}
+
+	if (!options.HasOptions)
+	{
+		if (GetConsoleWindow() || GetConsoleOutputCP())
+		{
+			// we have a console, or were created using CREATE_NO_WINDOW, in which case GetConsoleOutputCP() is not 0
+			options.Flags = 0;
+		}
 		else
 		{
-			hasOption = false;
+			// Plus: this automatically redirects output of grandchildren
+			// Con: this sets wrong code page for international output.
+			//
+			// Normally console applications use OEM codepage for standard output (CP866 for Russian), but detached procseses use ANSI codepage (CP1251 for Russian).
+			// Our parent invoked us as DETACHED_PROSESS, so it may expect the output in the system codepage (or may just not care).
+			// Our child process, being run as CREATE_NO_WINDOW, will produce output in OEM codepage, and our parent may not decode it correctly.
+			// If we had our own console, the child would inherit its encoding, but we are detached, and I don't know about a way to force ANSI code page on the child.
+			//
+			// Another sensible option would be to run the child detached ('-dr") as well. This ensures ANSI code page, but fails to redirect the output 
+			// of the grandchild process, if any
+			//
+			// If we want to preserve the code page AND account for the grandchildren, we must use an extra process that would switch to ANSI code page: 
+			// Exec -rw Exec -a child. However, this situation is rare. CREATE_NO_WINDOW+rediect would work just fine in most practical cases.
+
+			options.Flags = CREATE_NO_WINDOW;
+			options.ForceRedirect = true;
 		}
 	}
 
-	DWORD processFlags = flagsFromOption;
+	if (options.ForceAnsiCodepage)
+	{
+		SetConsoleOutputCP(GetACP());
+	}
 
 	STARTUPINFO si;
 	ZeroMemory(&si, sizeof(si));
 	si.cb = sizeof(si);
-	
-	if (!hasOption && !GetConsoleWindow())
-	{
-		// Process flags option not specified and there is no console
-		// Current process is started with either DETACHED_PROCESS or CREATE_NO_WINDOW.
-		// The distinction is important for handling non-ASCII output
-		// If we are DETACHED_PROCESS, we will have system code page CP_ACP, CP 1251 for Cyrillic
-		// If we are CREATE_NO_WINDOW, we will have OEM code page CP_OEMCP, CP 866 for Cyrillic
-		// 
-		// We want the child process to have the same I/O handles and the same code page as us, or the output will be garbled.
-		// Thus, we run it detached if we are detached, and "no window" otherwise
-		bool weAreDetached = GetConsoleOutputCP() == CP_ACP; 
-		processFlags = weAreDetached ? DETACHED_PROCESS : CREATE_NO_WINDOW;
 
+	if (options.ForceRedirect)
+	{
 		si.dwFlags = STARTF_USESTDHANDLES;
 		si.hStdInput = duplicate(GetStdHandle(STD_INPUT_HANDLE));
 		si.hStdOutput = duplicate(GetStdHandle(STD_OUTPUT_HANDLE));
 		si.hStdError = duplicate(GetStdHandle(STD_ERROR_HANDLE));
 	}
 
-	int skipArgs = hasOption ? 2 : 1;
+	int skipArgs = options.HasOptions ? 2 : 1;
 	int childArgc = argc - skipArgs;
 	wchar_t** childArgv = argv + skipArgs;
 	wstring command = ArgsUtil<wchar_t>::EscapeArgs(childArgc, childArgv);
 
 	PROCESS_INFORMATION pi;
 
-	if (!CreateProcessW(NULL, &command.front(), NULL, NULL, TRUE, processFlags, NULL, NULL, &si, &pi))
+	if (!CreateProcessW(NULL, &command.front(), NULL, NULL, TRUE, options.Flags, NULL, NULL, &si, &pi))
 	{
 		DWORD error = GetLastError();
 		return error;
